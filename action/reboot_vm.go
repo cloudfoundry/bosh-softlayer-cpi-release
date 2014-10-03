91865fb4b9b02e8d432076370c8720e68dd9478f
dr.max <maximilien@acm.org>
2014-10-02 17:53:14 -0700

    debugging code for reboot_vm action

--- action/reboot_vm.go
+++ action/reboot_vm.go
@@ -1,5 +1,9 @@
 package action
 
+import (
+	"fmt"
+)
+
 type RebootVM struct{}
 
 func NewRebootVM() RebootVM {
@@ -7,5 +11,11 @@ func NewRebootVM() RebootVM {
 }
 
 func (a RebootVM) Run(vmCID VMCID) (interface{}, error) {
+	//DEBUG
+	fmt.Println("RebootVM.Run")
+	fmt.Printf("----> vmCID: %#v\n", vmCID)
+	fmt.Println()
+	//DEBUG
+
 	return nil, nil
 }
