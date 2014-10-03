3291a1fc67862b59b746ab541f4dc78bf3d46cb9
dr.max <maximilien@acm.org>
2014-10-02 19:13:20 -0700

    added set_vm_metadata test code

--- action/set_vm_metadata.go
+++ action/set_vm_metadata.go
@@ -1,14 +1,29 @@
 package action
 
+import "fmt"
+
 type SetVMMetadata struct{}
 
-type VMMetadata struct{}
+type Bar struct {
+	Test int `json:"test"`
+}
+
+type VMMetadata struct {
+	Foo string `json:"foo,omitempty"`
+	Bar Bar    `json:"bar,omitempty"`
+}
 
 func NewSetVMMetadata() SetVMMetadata {
 	return SetVMMetadata{}
 }
 
 func (a SetVMMetadata) Run(vmCID VMCID, metadata VMMetadata) (interface{}, error) {
-	// todo can properties be set on the container
+	//DEBUG
+	fmt.Println("SetVMMetadata.Run")
+	fmt.Printf("----> vmCID: %#v\n", vmCID)
+	fmt.Printf("----> metadata: %#v\n", metadata)
+	fmt.Println()
+	//DEBUG
+
 	return nil, nil
 }
