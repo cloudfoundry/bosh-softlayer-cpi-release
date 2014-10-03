8a77aa8cf75ed19b2fabd0afadc722e4691ea5d3
dr.max <maximilien@acm.org>
2014-10-02 19:17:35 -0700

    added test configure_networks action

--- action/configure_networks.go
+++ action/configure_networks.go
@@ -1,5 +1,9 @@
 package action
 
+import (
+	"fmt"
+)
+
 type ConfigureNetworks struct{}
 
 func NewConfigureNetworks() ConfigureNetworks {
@@ -7,5 +11,12 @@ func NewConfigureNetworks() ConfigureNetworks {
 }
 
 func (a ConfigureNetworks) Run(vmCID VMCID, networks Networks) (interface{}, error) {
+	//DEBUG
+	fmt.Println("ConfigureNetworks.Run")
+	fmt.Printf("----> vmCID: %#v\n", vmCID)
+	fmt.Printf("----> networks: %#v\n", networks)
+	fmt.Println()
+	//DEBUG
+
 	return nil, nil
 }
