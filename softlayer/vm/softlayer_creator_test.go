12b093716d9613b1ddba0ba6e8f3fe084195d07c
dr.max <maximilien@acm.org>
2014-10-08 11:51:50 -0700

    added test for creator that uses fake SL client with test fixtures

--- softlayer/vm/softlayer_creator_test.go
+++ softlayer/vm/softlayer_creator_test.go
@@ -1,14 +1,23 @@
 package vm_test
 
 import (
+	"os"
+	"path/filepath"
+
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 
 	. "github.com/maximilien/bosh-softlayer-cpi/softlayer/vm"
 
-	boshlog "github.com/cloudfoundry/bosh-agent/logger"
-
 	fakevm "github.com/maximilien/bosh-softlayer-cpi/softlayer/vm/fakes"
 	fakeslclient "github.com/maximilien/softlayer-go/client/fakes"
+
+	common "github.com/maximilien/bosh-softlayer-cpi/common"
+	bslcstem "github.com/maximilien/bosh-softlayer-cpi/softlayer/stemcell"
+
+	boshlog "github.com/cloudfoundry/bosh-agent/logger"
+
+	sldatatypes "github.com/maximilien/softlayer-go/data_types"
 )
 
 var _ = Describe("SoftLayerCreator", func() {
@@ -21,7 +30,13 @@ var _ = Describe("SoftLayerCreator", func() {
 	)
 
 	BeforeEach(func() {
+		workingDir, err := os.Getwd()
+		Expect(err).ToNot(HaveOccurred())
+
 		softLayerClient = fakeslclient.NewFakeSoftLayerClient("fake-username", "fake-api-key")
+		softLayerClient.DoRawHttpRequestResponse, err = common.ReadJsonTestFixtures(filepath.Join(workingDir, "..", ".."), "softlayer", "SoftLayer_Virtual_Guest_Service_createObject.json")
+		Expect(err).ToNot(HaveOccurred())
+
 		agentEnvServiceFactory = &fakevm.FakeAgentEnvServiceFactory{}
 		agentOptions = AgentOptions{Mbus: "fake-mbus"}
 		logger = boshlog.NewLogger(boshlog.LevelNone)
@@ -35,5 +50,73 @@ var _ = Describe("SoftLayerCreator", func() {
 	})
 
 	Describe("Create", func() {
+		var (
+			agentID    string
+			stemcell   bslcstem.FSStemcell
+			cloudProps VMCloudProperties
+			networks   Networks
+			env        Environment
+		)
+
+		Context("valid arguments", func() {
+			BeforeEach(func() {
+				agentID = "fake-agent-id"
+				stemcell = bslcstem.NewFSStemcell("fake-stemcell-id", logger)
+				cloudProps = VMCloudProperties{
+					StartCpus:  4,
+					MaxMemory:  2048,
+					Datacenter: sldatatypes.Datacenter{Name: "fake-datacenter"},
+				}
+				networks = Networks{}
+				env = Environment{}
+			})
+
+			It("returns a new SoftLayerVM with correct virtual guest ID and SoftLayerClient", func() {
+				vm, err := creator.Create(agentID, stemcell, cloudProps, networks, env)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(vm.ID()).To(Equal(1234567))
+			})
+		})
+
+		Context("invalid arguments", func() {
+			Context("missing correct VMProperties", func() {
+				BeforeEach(func() {
+					agentID = "fake-agent-id"
+					stemcell = bslcstem.NewFSStemcell("fake-stemcell-id", logger)
+					networks = Networks{}
+					env = Environment{}
+				})
+
+				It("fails when VMProperties is missing StartCpus", func() {
+					cloudProps = VMCloudProperties{
+						MaxMemory:  2048,
+						Datacenter: sldatatypes.Datacenter{Name: "fake-datacenter"},
+					}
+
+					_, err := creator.Create(agentID, stemcell, cloudProps, networks, env)
+					Expect(err).To(HaveOccurred())
+				})
+
+				It("fails when VMProperties is missing MaxMemory", func() {
+					cloudProps = VMCloudProperties{
+						StartCpus:  4,
+						Datacenter: sldatatypes.Datacenter{Name: "fake-datacenter"},
+					}
+
+					_, err := creator.Create(agentID, stemcell, cloudProps, networks, env)
+					Expect(err).To(HaveOccurred())
+				})
+
+				It("fails when VMProperties is missing MaxMemory", func() {
+					cloudProps = VMCloudProperties{
+						StartCpus: 4,
+						MaxMemory: 1024,
+					}
+
+					_, err := creator.Create(agentID, stemcell, cloudProps, networks, env)
+					Expect(err).To(HaveOccurred())
+				})
+			})
+		})
 	})
 })
