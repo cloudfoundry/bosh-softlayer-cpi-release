692a8f3f252eb2dff7f314fea44067c51355f06b
dr.max <maximilien@acm.org>
2014-10-09 14:17:34 -0700

    added contexts to separate the various group of methods

--- action/concrete_factory_test.go
+++ action/concrete_factory_test.go
@@ -66,127 +66,136 @@ var _ = Describe("concreteFactory", func() {
 		)
 	})
 
-	It("returns error if action cannot be created", func() {
-		action, err := factory.Create("fake-unknown-action")
-		Expect(err).To(HaveOccurred())
-		Expect(action).To(BeNil())
+	Context("Stemcell methods", func() {
+		It("create_stemcell", func() {
+			action, err := factory.Create("create_stemcell")
+			Expect(err).ToNot(HaveOccurred())
+			Expect(action).To(Equal(NewCreateStemcell(stemcellFinder)))
+		})
+
+		It("delete_stemcell", func() {
+			action, err := factory.Create("delete_stemcell")
+			Expect(err).ToNot(HaveOccurred())
+			Expect(action).To(Equal(NewDeleteStemcell(stemcellFinder)))
+		})
 	})
 
-	It("create_stemcell", func() {
-		stemcellImporter := bslcstem.NewSoftLayerImporter(logger)
+	Context("VM methods", func() {
+		It("create_vm", func() {
+			vmCreator := bslcvm.NewSoftLayerCreator(
+				softLayerClient,
+				agentEnvServiceFactory,
+				options.Agent,
+				logger,
+			)
 
-		action, err := factory.Create("create_stemcell")
-		Expect(err).ToNot(HaveOccurred())
-		Expect(action).To(Equal(NewCreateStemcell(stemcellImporter)))
+			action, err := factory.Create("create_vm")
+			Expect(err).ToNot(HaveOccurred())
+			Expect(action).To(Equal(NewCreateVM(stemcellFinder, vmCreator)))
+		})
+
+		It("delete_vm", func() {
+			action, err := factory.Create("delete_vm")
+			Expect(err).ToNot(HaveOccurred())
+			Expect(action).To(Equal(NewDeleteVM(vmFinder)))
+		})
+
+		It("has_vm", func() {
+			action, err := factory.Create("has_vm")
+			Expect(err).ToNot(HaveOccurred())
+			Expect(action).To(Equal(NewHasVM(vmFinder)))
+		})
+
+		It("reboot_vm", func() {
+			action, err := factory.Create("reboot_vm")
+			Expect(err).ToNot(HaveOccurred())
+			Expect(action).To(Equal(NewRebootVM()))
+		})
+
+		It("set_vm_metadata", func() {
+			action, err := factory.Create("set_vm_metadata")
+			Expect(err).ToNot(HaveOccurred())
+			Expect(action).To(Equal(NewSetVMMetadata()))
+		})
+
+		It("configure_networks", func() {
+			action, err := factory.Create("configure_networks")
+			Expect(err).ToNot(HaveOccurred())
+			Expect(action).To(Equal(NewConfigureNetworks()))
+		})
 	})
 
-	It("delete_stemcell", func() {
-		action, err := factory.Create("delete_stemcell")
-		Expect(err).ToNot(HaveOccurred())
-		Expect(action).To(Equal(NewDeleteStemcell(stemcellFinder)))
+	//TODO: implement when disk methods and actions are added
+	XContext("Disk methods", func() {
+		It("create_disk", func() {
+			diskCreator := bslcdisk.NewFSCreator(
+				"/tmp/disks",
+				fs,
+				cmdRunner,
+				logger,
+			)
+
+			action, err := factory.Create("create_disk")
+			Expect(err).ToNot(HaveOccurred())
+			Expect(action).To(Equal(NewCreateDisk(diskCreator)))
+		})
+
+		It("delete_disk", func() {
+			action, err := factory.Create("delete_disk")
+			Expect(err).ToNot(HaveOccurred())
+			Expect(action).To(BeNil())
+		})
+
+		It("attach_disk", func() {
+			action, err := factory.Create("attach_disk")
+			Expect(err).ToNot(HaveOccurred())
+			Expect(action).To(BeNil())
+		})
+
+		It("detach_disk", func() {
+			action, err := factory.Create("detach_disk")
+			Expect(err).ToNot(HaveOccurred())
+			Expect(action).To(BeNil())
+		})
 	})
 
-	It("create_vm", func() {
-		vmCreator := bslcvm.NewSoftLayerCreator(
-			softLayerClient,
-			agentEnvServiceFactory,
-			options.Agent,
-			logger,
-		)
+	Context("Unsupported methods", func() {
+		It("returns error because CPI machine is not self-aware if action is current_vm_id", func() {
+			action, err := factory.Create("current_vm_id")
+			Expect(err).To(HaveOccurred())
+			Expect(action).To(BeNil())
+		})
 
-		action, err := factory.Create("create_vm")
-		Expect(err).ToNot(HaveOccurred())
-		Expect(action).To(Equal(NewCreateVM(stemcellFinder, vmCreator)))
+		It("returns error because snapshotting is not implemented if action is snapshot_disk", func() {
+			action, err := factory.Create("snapshot_disk")
+			Expect(err).To(HaveOccurred())
+			Expect(action).To(BeNil())
+		})
+
+		It("returns error because snapshotting is not implemented if action is delete_snapshot", func() {
+			action, err := factory.Create("delete_snapshot")
+			Expect(err).To(HaveOccurred())
+			Expect(action).To(BeNil())
+		})
+
+		It("returns error since CPI should not keep state if action is get_disks", func() {
+			action, err := factory.Create("get_disks")
+			Expect(err).To(HaveOccurred())
+			Expect(action).To(BeNil())
+		})
+
+		It("returns error because ping is not official CPI method if action is ping", func() {
+			action, err := factory.Create("ping")
+			Expect(err).To(HaveOccurred())
+			Expect(action).To(BeNil())
+		})
 	})
 
-	It("delete_vm", func() {
-		action, err := factory.Create("delete_vm")
-		Expect(err).ToNot(HaveOccurred())
-		Expect(action).To(Equal(NewDeleteVM(vmFinder)))
-	})
-
-	It("has_vm", func() {
-		action, err := factory.Create("has_vm")
-		Expect(err).ToNot(HaveOccurred())
-		Expect(action).To(Equal(NewHasVM(vmFinder)))
-	})
-
-	It("reboot_vm", func() {
-		action, err := factory.Create("reboot_vm")
-		Expect(err).ToNot(HaveOccurred())
-		Expect(action).To(Equal(NewRebootVM()))
-	})
-
-	It("set_vm_metadata", func() {
-		action, err := factory.Create("set_vm_metadata")
-		Expect(err).ToNot(HaveOccurred())
-		Expect(action).To(Equal(NewSetVMMetadata()))
-	})
-
-	It("configure_networks", func() {
-		action, err := factory.Create("configure_networks")
-		Expect(err).ToNot(HaveOccurred())
-		Expect(action).To(Equal(NewConfigureNetworks()))
-	})
-
-	XIt("create_disk", func() {
-		diskCreator := bslcdisk.NewFSCreator(
-			"/tmp/disks",
-			fs,
-			cmdRunner,
-			logger,
-		)
-
-		action, err := factory.Create("create_disk")
-		Expect(err).ToNot(HaveOccurred())
-		Expect(action).To(Equal(NewCreateDisk(diskCreator)))
-	})
-
-	XIt("delete_disk", func() {
-		action, err := factory.Create("delete_disk")
-		Expect(err).ToNot(HaveOccurred())
-		Expect(action).To(BeNil())
-	})
-
-	XIt("attach_disk", func() {
-		action, err := factory.Create("attach_disk")
-		Expect(err).ToNot(HaveOccurred())
-		Expect(action).To(BeNil())
-	})
-
-	XIt("detach_disk", func() {
-		action, err := factory.Create("detach_disk")
-		Expect(err).ToNot(HaveOccurred())
-		Expect(action).To(BeNil())
-	})
-
-	It("returns error because CPI machine is not self-aware if action is current_vm_id", func() {
-		action, err := factory.Create("current_vm_id")
-		Expect(err).To(HaveOccurred())
-		Expect(action).To(BeNil())
-	})
-
-	It("returns error because snapshotting is not implemented if action is snapshot_disk", func() {
-		action, err := factory.Create("snapshot_disk")
-		Expect(err).To(HaveOccurred())
-		Expect(action).To(BeNil())
-	})
-
-	It("returns error because snapshotting is not implemented if action is delete_snapshot", func() {
-		action, err := factory.Create("delete_snapshot")
-		Expect(err).To(HaveOccurred())
-		Expect(action).To(BeNil())
-	})
-
-	It("returns error since CPI should not keep state if action is get_disks", func() {
-		action, err := factory.Create("get_disks")
-		Expect(err).To(HaveOccurred())
-		Expect(action).To(BeNil())
-	})
-
-	It("returns error because ping is not official CPI method if action is ping", func() {
-		action, err := factory.Create("ping")
-		Expect(err).To(HaveOccurred())
-		Expect(action).To(BeNil())
+	Context("Misc", func() {
+		It("returns error if action cannot be created", func() {
+			action, err := factory.Create("fake-unknown-action")
+			Expect(err).To(HaveOccurred())
+			Expect(action).To(BeNil())
+		})
 	})
 })
