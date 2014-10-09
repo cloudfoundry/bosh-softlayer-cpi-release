bbb0ca365c9e438727531905bc5c0662a412e355
dr.max <maximilien@acm.org>
2014-10-09 14:19:58 -0700

    completed the softlayer stemcell finder

--- softlayer/stemcell/softlayer_finder.go
+++ softlayer/stemcell/softlayer_finder.go
@@ -22,6 +22,24 @@ func (f SoftLayerFinder) Find(id string) (Stemcell, bool, error) {
 		return nil, false, bosherr.WrapError(err, "Getting SoftLayer AccountService")
 	}
 
+	stemcell, found, err := f.findInVirtualDiskImages(id, accountService)
+	if err != nil {
+		return stemcell, found, err
+	}
+
+	if found {
+		return stemcell, found, nil
+	} else {
+		stemcell, found, err = f.findInVirtualGuestDeviceTemplateGroups(id, accountService)
+		if err != nil {
+			return stemcell, found, err
+		}
+	}
+
+	return stemcell, found, nil
+}
+
+func (f SoftLayerFinder) findInVirtualDiskImages(id string, accountService sl.SoftLayer_Account_Service) (Stemcell, bool, error) {
 	virtualDiskImages, err := accountService.GetVirtualDiskImages()
 	if err != nil {
 		return nil, false, bosherr.WrapError(err, "Getting virtual disk images")
@@ -29,6 +47,21 @@ func (f SoftLayerFinder) Find(id string) (Stemcell, bool, error) {
 
 	for _, vdImage := range virtualDiskImages {
 		if vdImage.Uuid == id {
+			return NewSoftLayerStemcell(id, f.logger), true, nil
+		}
+	}
+
+	return nil, false, nil
+}
+
+func (f SoftLayerFinder) findInVirtualGuestDeviceTemplateGroups(id string, accountService sl.SoftLayer_Account_Service) (Stemcell, bool, error) {
+	vgdtgGroups, err := accountService.GetBlockDeviceTemplateGroups()
+	if err != nil {
+		return nil, false, bosherr.WrapError(err, "Getting virtual guest device template groups")
+	}
+
+	for _, vgdtgGroup := range vgdtgGroups {
+		if vgdtgGroup.GlobalIdentifier == id {
 			return NewSoftLayerStemcell(id, f.logger), true, nil
 		}
 	}
