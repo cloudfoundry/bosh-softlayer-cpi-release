a5d6a521c8d322c28fe3560e77eb786f4b584d92
xingzhou <xingzhou@cn.ibm.com>
2015-01-04 17:27:26 +0800

    Add one new action to establish baremetal server eCPI environment
    In this patch, we added the following features:
    
    1. Add a new action named "establish_bare_metal_env" to set up a eCPI env on a newly created bare_metal server.
    
    2. Finished the first step of "establish_bare_metal_env" action which is to use softlayer-go's hardware service to create a new baremetal server.
    
    Signed-off-by: xingzhou <xingzhou@cn.ibm.com>

---
+++ softlayer/baremetal/baremetal_finder_test.go
@@ -0,0 +1,59 @@
+package baremetal_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	boshlog "github.com/cloudfoundry/bosh-agent/logger"
+	common "github.com/maximilien/bosh-softlayer-cpi/common"
+	bm "github.com/maximilien/bosh-softlayer-cpi/softlayer/baremetal"
+	fakeslclient "github.com/maximilien/softlayer-go/client/fakes"
+)
+
+var _ = Describe("BaremetalFinder", func() {
+	var (
+		softLayerClient *fakeslclient.FakeSoftLayerClient
+		logger          boshlog.Logger
+		finder          bm.BaremetalFinder
+	)
+
+	BeforeEach(func() {
+		softLayerClient = fakeslclient.NewFakeSoftLayerClient("fake-username", "fake-api-key")
+		logger = boshlog.NewLogger(boshlog.LevelNone)
+
+		finder = bm.NewBaremetalFinder(
+			softLayerClient,
+			logger,
+		)
+	})
+
+	Describe("Find", func() {
+		Context("succeeded", func() {
+			BeforeEach(func() {
+				common.SetTestFixtureForFakeSoftLayerClient(softLayerClient, "SoftLayer_Hardware_Service_getObject.json")
+			})
+
+			It("returns a new Softlayer Hardware without an error", func() {
+				baremetal, err := finder.Find("fake-id")
+				Expect(err).ToNot(HaveOccurred())
+				Expect(baremetal.GlobalIdentifier).To(Equal("fake-id"))
+				Expect(baremetal.BareMetalInstanceFlag).To(Equal(1))
+				Expect(baremetal.ProvisionDate).ToNot(BeNil())
+				Expect(baremetal.PrimaryIpAddress).To(Equal("1.1.1.1"))
+			})
+		})
+
+		Context("failed", func() {
+			BeforeEach(func() {
+				common.SetTestFixtureForFakeSoftLayerClient(softLayerClient, "SoftLayer_Hardware_Service_getObject_None_Exist.json")
+			})
+
+			It("return an error when the specified hardward id can not be found", func() {
+				_, err := finder.Find("none-exist-id")
+				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(Equal("can not find the baremetal server with id: none-exist-id."))
+			})
+
+		})
+	})
+})
