179ca5f245ce45a1f1c44903ec93de53c933de5b
Kara Felix <karairenefelix@gmail.com>
2015-11-02 11:24:28 -0800

    cleaned up some missing pieces in the VmNamePrefix -> Hostname refactor

--- softlayer/vm/vm_utils_test.go
+++ softlayer/vm/vm_utils_test.go
@@ -204,7 +204,7 @@ var _ = Describe("VM Utils", func() {
 				Datacenter:                   sldatatypes.Datacenter{Name: "fake-datacenter"},
 				HourlyBillingFlag:            true,
 				LocalDiskFlag:                true,
-				VmNamePrefix:                 "bosh-",
+				Hostname:                     "bosh-",
 				PostInstallScriptUri:         "",
 				DedicatedAccountHostOnlyFlag: true,
 				PrivateNetworkOnlyFlag:       false,
