9aa99bd7ecb81414a9605bb3bdf26eaa383fe54b
Matt Cui <mattcui@gmail.com>
2016-01-05 22:42:49 +0800

    Increase timeout

--- softlayer/vm/pool/db_util.go
+++ softlayer/vm/pool/db_util.go
@@ -16,8 +16,8 @@ var (
 	SQLITE_DB_FOLDER    string
 	SQLITE_DB_FILE      string
 	SQLITE_DB_FILE_PATH string
-	DB_RETRY_INTERVAL   = 1 * time.Second
-	DB_RETRY_TIMEOUT    = 30 * time.Second
+	DB_RETRY_INTERVAL   = 3 * time.Second
+	DB_RETRY_TIMEOUT    = 60 * time.Second
 )
 
 func InitVMPoolDB(retryTimeout time.Duration, retryInterval time.Duration, logger boshlog.Logger) error {
