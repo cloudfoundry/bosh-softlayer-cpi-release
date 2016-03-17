d3843e5b77c3dfa93b209b3bd946e1cf6d347133
jianqiu <wangjq@cn.ibm.com>
2016-03-17 16:05:59 +0800

    update

--- common/utility.go
+++ common/utility.go
@@ -33,27 +33,27 @@ type ipRange struct {
 var privateRanges = []ipRange{
 	ipRange{
 		start: net.ParseIP("10.0.0.0"),
-		end: net.ParseIP("10.255.255.255"),
+		end:   net.ParseIP("10.255.255.255"),
 	},
 	ipRange{
 		start: net.ParseIP("100.64.0.0"),
-		end: net.ParseIP("100.127.255.255"),
+		end:   net.ParseIP("100.127.255.255"),
 	},
 	ipRange{
 		start: net.ParseIP("172.16.0.0"),
-		end: net.ParseIP("172.31.255.255"),
+		end:   net.ParseIP("172.31.255.255"),
 	},
 	ipRange{
 		start: net.ParseIP("192.0.0.0"),
-		end: net.ParseIP("192.0.0.255"),
+		end:   net.ParseIP("192.0.0.255"),
 	},
 	ipRange{
 		start: net.ParseIP("192.168.0.0"),
-		end: net.ParseIP("192.168.255.255"),
+		end:   net.ParseIP("192.168.255.255"),
 	},
 	ipRange{
 		start: net.ParseIP("198.18.0.0"),
-		end: net.ParseIP("198.19.255.255"),
+		end:   net.ParseIP("198.19.255.255"),
 	},
 }
 
@@ -69,7 +69,7 @@ func IsPrivateSubnet(ipAddress net.IP) bool {
 }
 
 func inRange(r ipRange, ipAddress net.IP) bool {
-	if bytes.Compare(ipAddress, r.start) >= 0 && bytes.Compare(ipAddress, r.end) {
+	if bytes.Compare(ipAddress, r.start) >= 0 && bytes.Compare(ipAddress, r.end) <= 0 {
 		return true
 	}
 	return false
