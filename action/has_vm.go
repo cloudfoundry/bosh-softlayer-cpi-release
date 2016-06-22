e143af896ff9bff21e1cdc8dc09bf6049941936c
jianqiu <wangjq@cn.ibm.com>
2016-06-22 16:29:34 +0800

    has_vm action should not return error which might break bosh deploy or bosh cck

--- action/has_vm.go
+++ action/has_vm.go
@@ -20,7 +20,7 @@ func NewHasVM(
 func (a HasVMAction) Run(vmCID VMCID) (bool, error) {
 	_, found, err := a.vmFinder.Find(int(vmCID))
 	if err != nil {
-		return false, bosherr.WrapErrorf(err, "Finding VM '%d'", vmCID)
+		return false, nil
 	}
 
 	return found, nil
