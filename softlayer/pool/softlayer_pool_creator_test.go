283136096800d576442ae6431d1c98931d00a131
jianqiu <wangjq@cn.ibm.com>
2016-11-22 12:44:47 +0800

    initialize testcases for pool creator,deleter and virtual guest deleter

---
+++ softlayer/pool/softlayer_pool_creator_test.go
@@ -0,0 +1,12 @@
+package pool_test
+
+import (
+	. "github.com/cloudfoundry/bosh-softlayer-cpi/softlayer/pool"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("SoftlayerPoolCreator", func() {
+
+})
---
+++ softlayer/vm/softlayer_virtual_guest_deleter_test.go
@@ -0,0 +1,12 @@
+package vm_test
+
+import (
+	. "github.com/cloudfoundry/bosh-softlayer-cpi/softlayer/vm"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("SoftlayerVirtualGuestDeleter", func() {
+
+})
