05a4b0b119b213613745bcc3f64a0126f359152c
Zida <shawzida@gmail.com>
2017-11-27 15:39:17 +0800

    packages: update to Golang 1.8.5; docs: add runbook of distinguish logs; integration: comment flavor stuffs

--- src/bosh-softlayer-cpi/integration/vm_test.go
+++ src/bosh-softlayer-cpi/integration/vm_test.go
@@ -128,52 +128,52 @@ var _ = Describe("VM", func() {
 		assertSucceeds(request)
 	})
 
-	It("can create a VM by flavor", func() {
-		var vmCID string
-		By("creating a VM by flavor")
-		request := fmt.Sprintf(`{
-		  "method": "create_vm",
-		  "arguments": [
-		    "45632666-9fb1-422a-af35-2ab6102c5c1b",
-		    "%v",
-		    {
-		      "hostname_prefix": "bluebosh-slcpi-integration-test",
-		      "domain": "softlayer.com",
-		      "flavor_key_name": "B1_1X2X25",
-		      "ephemeral_disk_size": 25,
-		      "max_network_speed": 100,
-		      "hourly_billing_flag": true,
-		      "dedicated_account_host_only_flag": false,
-		      "datacenter": "lon02"
-		    },
-		    {
-		      "default": {
-		        "type": "dynamic",
-		        "dns": [
-			      "8.8.8.8"
-		        ],
-		        "default": [
-		          "dns",
-		          "gateway"
-		        ],
-		        "cloud_properties": {
-		          "vlan_ids": [1292653, 1292651]
-		        }
-		      }
-		    },
-		    null,
-		    {}
-		  ]
-		}`, existingStemcellId)
-		vmCID = assertSucceedsWithResult(request).(string)
-
-		By("deleting the VM")
-		request = fmt.Sprintf(`{
-		  "method": "delete_vm",
-		  "arguments": ["%v"]
-		}`, vmCID)
-		assertSucceeds(request)
-	})
+	//XIt("can create a VM by flavor", func() {
+	//	var vmCID string
+	//	By("creating a VM by flavor")
+	//	request := fmt.Sprintf(`{
+	//	  "method": "create_vm",
+	//	  "arguments": [
+	//	    "45632666-9fb1-422a-af35-2ab6102c5c1b",
+	//	    "%v",
+	//	    {
+	//	      "hostname_prefix": "bluebosh-slcpi-integration-test",
+	//	      "domain": "softlayer.com",
+	//	      "flavor_key_name": "B1_1X2X25",
+	//	      "ephemeral_disk_size": 25,
+	//	      "max_network_speed": 100,
+	//	      "hourly_billing_flag": true,
+	//	      "dedicated_account_host_only_flag": false,
+	//	      "datacenter": "lon02"
+	//	    },
+	//	    {
+	//	      "default": {
+	//	        "type": "dynamic",
+	//	        "dns": [
+	//		      "8.8.8.8"
+	//	        ],
+	//	        "default": [
+	//	          "dns",
+	//	          "gateway"
+	//	        ],
+	//	        "cloud_properties": {
+	//	          "vlan_ids": [1292653, 1292651]
+	//	        }
+	//	      }
+	//	    },
+	//	    null,
+	//	    {}
+	//	  ]
+	//	}`, existingStemcellId)
+	//	vmCID = assertSucceedsWithResult(request).(string)
+	//
+	//	By("deleting the VM")
+	//	request = fmt.Sprintf(`{
+	//	  "method": "delete_vm",
+	//	  "arguments": ["%v"]
+	//	}`, vmCID)
+	//	assertSucceeds(request)
+	//})
 
 	It("can create a VM with static ip", func() {
 		By("creating a VM with static ip")
@@ -199,7 +199,7 @@ var _ = Describe("VM", func() {
 		      "default": {
 				"cloud_properties": {
 				  "vlan_ids": [
-					524954
+					1292651
 				  ]
 				},
 				"dns": [
