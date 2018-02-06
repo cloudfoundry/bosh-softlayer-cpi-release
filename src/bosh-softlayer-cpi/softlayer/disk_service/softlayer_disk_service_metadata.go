bd0c71c3cbc43d698ae2b76de164b0f9bed489e1
Zida <shawzida@gmail.com>
2018-02-06 09:39:12 +0800

    Format disk metadata string

--- src/bosh-softlayer-cpi/softlayer/disk_service/softlayer_disk_service_metadata.go
+++ src/bosh-softlayer-cpi/softlayer/disk_service/softlayer_disk_service_metadata.go
@@ -28,37 +28,40 @@ func (d SoftlayerDiskService) SetMetadata(id int, vmMetadata Metadata) error {
 func (d SoftlayerDiskService) generateNotesFromDiskMetadata(vmMetadata Metadata) (string, error) {
 	var tagStringBuffer bytes.Buffer
 	if val, ok := vmMetadata["director"]; ok {
+		tagStringBuffer.WriteString("director" + ": " + val.(string))
 		tagStringBuffer.WriteString(", ")
-		tagStringBuffer.WriteString("director" + ":" + val.(string))
 	}
 
 	if val, ok := vmMetadata["deployment"]; ok {
-		tagStringBuffer.WriteString("deployment" + ":" + val.(string))
+		tagStringBuffer.WriteString("deployment" + ": " + val.(string))
+		tagStringBuffer.WriteString(", ")
 	}
 
 	if val, ok := vmMetadata["instance_id"]; ok {
-		tagStringBuffer.WriteString("instance_id" + ":" + val.(string))
+		tagStringBuffer.WriteString("instance_id" + ": " + val.(string))
+		tagStringBuffer.WriteString(", ")
 	}
 
 	if val, ok := vmMetadata["job"]; ok {
+		tagStringBuffer.WriteString("job" + ": " + val.(string))
 		tagStringBuffer.WriteString(", ")
-		tagStringBuffer.WriteString("job" + ":" + val.(string))
 	}
 
 	if val, ok := vmMetadata["instance_index"]; ok {
+		tagStringBuffer.WriteString("instance_index" + ": " + val.(string))
 		tagStringBuffer.WriteString(", ")
-		tagStringBuffer.WriteString("instance_index" + ":" + val.(string))
 	}
 
 	if val, ok := vmMetadata["instance_name"]; ok {
+		tagStringBuffer.WriteString("instance_name" + ": " + val.(string))
 		tagStringBuffer.WriteString(", ")
-		tagStringBuffer.WriteString("instance_name" + ":" + val.(string))
 	}
 
 	if val, ok := vmMetadata["attached_at"]; ok {
+		tagStringBuffer.WriteString("attached_at" + ": " + val.(string))
 		tagStringBuffer.WriteString(", ")
-		tagStringBuffer.WriteString("attached_at" + ":" + val.(string))
 	}
+	tagStringBuffer.Truncate(tagStringBuffer.Len() - 2)
 
 	return tagStringBuffer.String(), nil
 }
