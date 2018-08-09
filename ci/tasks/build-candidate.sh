190212ba261ebf77d41735d6fd7ec8b465c67cdd
Zida <shawzida@gmail.com>
2018-08-09 20:58:54 +0800

    Change jobs name for multi cpis

--- ci/tasks/build-candidate.sh
+++ ci/tasks/build-candidate.sh
@@ -13,22 +13,13 @@ pushd bosh-cpi-release
 
   echo $semver > src/bosh-softlayer-cpi/version
 
-  cpi_release_name="bosh-softlayer-cpi"
-  tarball_name="dev_releases/${cpi_release_name}/${cpi_release_name}-${semver}.tgz"
+  cpi_release_filename="bosh-softlayer-cpi"
+  cpi_release_name="bosh-softlayer-legacy-cpi"
+  tarball_name="dev_releases/${cpi_release_filename}/${cpi_release_name}-${semver}.tgz"
+  mkdir -p "dev_releases/${cpi_release_filename}/"
   echo "building CPI release..."
 
   $BOSH_CLI create-release --name $cpi_release_name --version $semver --tarball $tarball_name --force
 popd
 
 mv bosh-cpi-release/$tarball_name candidate/
-
-
-
-
-
-
-
-
-
-
-
