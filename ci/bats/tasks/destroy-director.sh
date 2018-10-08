9d66b88e96f64d0f602ac7d0f96d6cad15a2f14c
Zida <shawzida@gmail.com>
2018-10-08 11:45:45 +0800

    Create an empty VM prior to deploying director

--- ci/bats/tasks/destroy-director.sh
+++ ci/bats/tasks/destroy-director.sh
@@ -2,22 +2,18 @@
 
 set -eu
 
-mv director-state/.bosh $HOME/
+mv director-state/.bosh "$HOME"/
 
 state_path() { bosh-cli int director-state/director.yml --path="$1" ; }
 
-function get_bosh_environment {
-  if [[ -z $(state_path /instance_groups/name=bosh/networks/name=public/static_ips/0 2>/dev/null) ]]; then
-    state_path /instance_groups/name=bosh/networks/name=default/static_ips/0 2>/dev/null
-  else
-    state_path /instance_groups/name=bosh/networks/name=public/static_ips/0 2>/dev/null
-  fi
-}
+BOSH_ENVIRONMENT=$( jq -r ".current_ip" director-state/director-state.json )
+BOSH_CA_CERT=$( bosh-cli int director-state/director-creds.yml --path /director_ssl/ca )
+BOSH_CLIENT_SECRET=$( bosh-cli int director-state/director-creds.yml --path /admin_password )
 
-export BOSH_ENVIRONMENT=`get_bosh_environment`
-export BOSH_CA_CERT=`bosh-cli int director-state/director-creds.yml --path /director_ssl/ca`
+export BOSH_ENVIRONMENT
+export BOSH_CA_CERT
 export BOSH_CLIENT=admin
-export BOSH_CLIENT_SECRET=`bosh-cli int director-state/director-creds.yml --path /admin_password`
+export BOSH_CLIENT_SECRET
 
 set +e
 
@@ -26,8 +22,8 @@ bosh-cli clean-up -n --all
 
 if [ -f "director-state/director.yml" ] && [ -f "director-state/director-creds.yml" ]
 then
-	echo -e "\n\033[32m[INFO] Director manifest found, deleting director environment.\033[0m"
+	echo -e "\\n\\033[32m[INFO] Director manifest found, deleting director environment.\\033[0m"
 	bosh-cli delete-env -n director-state/director.yml -l director-state/director-creds.yml
 else
-	echo -e "\n\033[33m[WARNING] Director manifest not found, You need check manually.\033[0m"
+	echo -e "\\n\\033[33m[WARNING] Director manifest not found, You need check manually.\\033[0m"
 fi
